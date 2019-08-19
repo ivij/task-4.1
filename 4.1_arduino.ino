8a1ff4cec6e08cb750af9f5ed590ae4628eb09ce
ivij <53103290+ivij@users.noreply.github.com>
2019-08-19 12:05:33 +1000

    Add files via upload

---
+++ 4.1_arduino.ino
@@ -0,0 +1,42 @@
+# Sensor value description
+
+  # 0 - 300     dry soil
+
+  # 300 - 700     humid soil
+
+  # 700 - 950     in water
+
+  */
+
+void setup() {
+
+  Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+
+  int val;
+
+  val = analogRead(0); //connect sensor to Analog 0
+
+  Serial.println(val); 
+  
+  if(val<=300)
+
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(1000);                       
+
+  }
+
+  else
+
+  {
+    digitalWrite(LED_BUILTIN, LOW);   
+    delay(1000);                       
+  }
+
+}
